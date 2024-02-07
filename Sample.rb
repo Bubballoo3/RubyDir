92500e5b04af55a8590baa3cc2067ced38f04a8b
Braeden <singleton1@kenyon.edu>
2024-02-07 15:40:37 -0500

    Sharing Unfinished Files

--- Sample.rb
+++ Sample.rb
@@ -1,6 +1,6 @@
 =begin 
 Ruby program to find Area of Rectangle.
-=end
+
 # input length and breadth, and 
 # convert them to float value
 puts "Enter length:"
@@ -11,3 +11,89 @@ w=gets.chomp.to_f
 area=l*w
 # printing the result
 puts "Area of Rectangle is #{area}"
+=end
+def parseSlideRange(string)
+    #this will be our array that we return at the end
+    slidesMentioned=Array.new
+
+    collectionsMentioned=Array.new
+    collectionsToIndex=Hash.new
+  
+    #we begin by splitting the ranges. 
+    #Ranges are separated by commas, and common info is not repeated
+    #an especially complicated example of this is 
+    #"B27.012-15, B45.905-06, B47.654-63, 716-18"
+    if string.include? ". "
+        n=string.index ". "
+        string=string[...n]
+    end
+
+    ranges=string.split(",",-1)
+    
+    for i in 0...ranges.length
+        ranges[i] = ranges[i].lstrip
+    end
+    #next we store each B-collection in case the next one reuses it
+    lastcollection="ERROR"
+
+    #we now loop through the ranges and process them
+    for i in 0...ranges.length
+      range=ranges[i]
+      puts range
+      
+      #the following will be a sample range to indicate which parts the code is handling
+      
+      #B22.222-22  
+      #   ^
+      if range.include? "."
+        decimalpoint=range.index "."
+        nextpoint=decimalpoint+1
+        rightside=range[nextpoint..]
+      else
+        #in case it is only 222-22 
+        decimalpoint=0
+        rightside=range
+      end
+      
+      #B22.222-22
+      #^^^
+
+      if range[0]=="B"
+        lastcollection=range[...decimalpoint]
+        unless collectionsMentioned.include? lastcollection
+          collectionsMentioned.push lastcollection
+        end 
+      end
+      
+      #B22.222-22
+      #       ^
+      dashplace=rightside.index "-"
+      
+      if dashplace < 3
+        rightside="0" + rightside
+        if dashplace < 2
+            rightside="0"+rightside
+        end
+      end
+      dashplace=4
+      hundreds=rightside[0]
+      start=rightside[1..2].to_i
+      last=rightside[dashplace..].to_i
+      for i in start..last
+        slidestem=lastcollection + "." + hundreds
+        if i.to_s.length < 2
+            ending="0"+i.to_s
+        else 
+            ending=i.to_s
+        end
+        slide=slidestem+ending
+        slidesMentioned.push slide
+      end
+      
+
+      puts "resolved"
+    end
+    return slidesMentioned
+    #we begin by splitting our description up by subcollection. 
+  end 
+  #parseSlideRange "B27.012-15, B47.654-63, 716-18,B45.9-10, B45.63-67. WHat the"
--- kmlParser.rb
+++ kmlParser.rb
@@ -156,21 +156,29 @@ end
 def splitLocations (stringLocation)
   if stringLocation.class != String
     return ["there has been an error","like actually"]
+  elsif (stringLocation.include? ",")==false
+    return ["there has been an error","like actually"]
   else
     commaSpot=stringLocation.index ","
-    latitude=stringLocation[...commaSpot]
-    longitude=stringLocation[(commaSpot+1)..]
-    return [latitude,longitude]
+    longitude=stringLocation[...commaSpot]
+    latitude=stringLocation[(commaSpot+1)..]
+    return [longitude,latitude]
   end
 end 
 
 
-def writeToXls(bigarray, filename="blank")
+def writeToXls(bigarray, mode="straight", filename="blank")
   #this function makes heavy use of the spreadsheet package. To install, type "gem install spreadsheet" into your terminal (windows)
   # or visit the source at https://rubygems.org/gems/spreadsheet/versions/1.3.0?locale=en
   require "spreadsheet" 
   #Next we set the encoding. This is the default setting but can be changed here
   Spreadsheet.client_encoding='UTF-8'
+  
+  #Now we define a mode. Each mode will direct the function to a different loop to produce different types of data.
+  #"straight" mode keeps data organized by location, ex. Baly Cottage => B43.32-53,location
+  #"CatNum" mode interprets each range and re-organizes it to read B43.32 => Baly Cottage, location
+
+
 
   #we now create our spreadsheet file
   book=Spreadsheet::Workbook.new
@@ -183,33 +191,86 @@ def writeToXls(bigarray, filename="blank")
   #we define a disclaimer to populate the top left cell, identifying that it was produced by code
   disclaimer="This is an automatically generated spreadsheet titled \'" + collectionTitle + ".\' Please review the information before copying into permanent data storage."
   mainsheet[0,0] = disclaimer
+  
+  if mode=="straight"
 
-  #then we make titles for each column
-  mainsheet[1,0]="Title"
-  mainsheet[1,1]="Description"
-  mainsheet[1,2]="Latitude"
-  mainsheet[1,3]="Longitude"
+    #then we make titles for each column
+    mainsheet[1,0]="Title"
+    mainsheet[1,1]="Description"
+    mainsheet[1,2]="Longitude"
+    mainsheet[1,3]="Latitude"
 
-  #with our title and disclaimer made, we move into our main loop
-  #the writing will take place one row at a time, and will be based on the list of keys (bigarray[1])
+    #with our title and disclaimer made, we move into our main loop
+    #the writing will take place one row at a time, and will be based on the list of keys (bigarray[1])
 
-  for i in 2..bigarray[1].length
-    #gather info
-    title = bigarray[1][i]
-    description=bigarray[2][title]
-    location=bigarray[3][title]
+    for i in 2..bigarray[1].length
+      #gather info
+      title = bigarray[1][i]
+      description=bigarray[2][title]
+      location=bigarray[3][title]
     
-    #while most of the data is ready to input, the locations are still a string tuple.
-    #we must split this into its parts before entry
+      #while most of the data is ready to input, the locations are still a string tuple.
+      #we must split this into its parts before entry
 
-    locationTuple = splitLocations location
+      locationTuple = splitLocations location
 
-    #populate info
-    mainsheet[i,0]=title
-    mainsheet[i,1]=description
-    mainsheet[i,2]=locationTuple[0]
-    mainsheet[i,3]=locationTuple[1]
-  end 
+      #populate info
+      mainsheet[i,0]=title
+      mainsheet[i,1]=description
+      mainsheet[i,2]=locationTuple[0]
+      mainsheet[i,3]=locationTuple[1]
+    end 
+  end
+
+  if mode == "CatNum"
+    titlesToMentions=Hash.new
+    upperslides=Array.new
+    lowerslides=Array.new
+    for i in 0...bigarray[1].length
+      activetitle=bigarray[1][i]
+      desc = bigarray[2][activetitle]
+      puts desc
+      
+      if desc.class != NilClass
+
+        if desc.include? "-"
+          smallerarray = parseSlideRange desc
+          lowerslides.push smallerarray[1]
+          upperslides.push smallerarray[2]
+          titlesToMentions[activetitle]=smallerarray[0]
+        elsif desc.length > 5
+          slide=desc[..6]
+          lowerslides.push slide
+          upperslides.push slide
+          titlesToMentions[activetitle]=[slide]
+        end
+      end
+    end
+    lastblock=2
+
+    #populate spreadsheet
+    mainsheet[1,0]="Cat#"
+    mainsheet[1,1]="Slide Title"
+    mainsheet[1,2]="Longitude"
+    mainsheet[1,3]="Latitude"
+
+    for i in 0...bigarray[1].length
+      activetitle=bigarray[1][i]
+      workinglist=titlesToMentions[activetitle]
+      location=bigarray[3][activetitle]
+      locationTuple=splitLocations location
+      if workinglist.class != NilClass
+        for j in 0...workinglist.length
+          mainsheet[lastblock,0]=workinglist[j]
+          mainsheet[lastblock,1]=activetitle
+          mainsheet[lastblock,2]=locationTuple[0]
+          mainsheet[lastblock,3]=locationTuple[1]
+          lastblock=lastblock+1
+        end
+      end
+    end
+  end
+
 
   if filename != "blank"
     book.write filename
@@ -222,6 +283,103 @@ def writeToXls(bigarray, filename="blank")
   end
 end
 
+def parseSlideRange(string)
+  #this will be our array that we return at the end
+  slidesMentioned=Array.new
+
+  collectionsMentioned=Array.new
+  collectionsToIndex=Hash.new
+
+  #we begin by splitting the ranges. 
+  #Ranges are separated by commas, and common info is not repeated
+  #an especially complicated example of this is 
+  #"B27.012-15, B45.905-06, B47.654-63, 716-18"
+  if string.include? ". "
+      n=string.index ". "
+      string=string[...n]
+  end
+
+  ranges=string.split(",",-1)
+  
+  for i in 0...ranges.length
+      ranges[i] = ranges[i].lstrip
+  end
+  #next we store each B-collection in case the next one reuses it
+  lastcollection="ERROR"
+
+  #we now loop through the ranges and process them
+  for i in 0...ranges.length
+    range=ranges[i]
+    
+    #the following will be a sample range to indicate which parts the code is handling
+    
+    #B22.222-22  
+    #   ^
+    if range.include? "."
+      decimalpoint=range.index "."
+      nextpoint=decimalpoint+1
+      rightside=range[nextpoint..]
+    else
+      #in case it is only 222-22 
+      decimalpoint=0
+      rightside=range
+    end
+    
+    #B22.222-22
+    #^^^
+
+    if range[0]=="B"
+      lastcollection=range[...decimalpoint]
+      unless collectionsMentioned.include? lastcollection
+        collectionsMentioned.push lastcollection
+      end 
+    end
+    
+    #B22.222-22
+    #       ^
+    if rightside.include? "-"
+      dashplace=rightside.index "-"
+    
+      if dashplace < 3
+        rightside="0" + rightside
+        if dashplace < 2
+          rightside="0"+rightside
+        end
+      end
+      dashplace=4
+      hundreds=rightside[0]
+      start=rightside[1..2].to_i
+      last=rightside[dashplace..].to_i
+      for i in start..last
+        slidestem=lastcollection + "." + hundreds
+        if i.to_s.length < 2
+          ending="0"+i.to_s
+        else 
+          ending=i.to_s
+        end
+        slide=slidestem+ending
+        slidesMentioned.push slide
+      end
+    else 
+      slide=lastcollection+"."+rightside
+      slidesMentioned.push slide
+    end
+  end
+
+  slidesMentioned=slidesMentioned.sort
+  minslide=slidesMentioned[0]
+  maxslide=slidesMentioned[-1]
+  return [slidesMentioned,minslide,maxslide]
+  #we begin by splitting our description up by subcollection. 
+end 
+#puts parseSlideRange "B45.321 approximate location at 35 degrees N"
+#"B27.012-15, B47.654-63, 716-18,B45.9-10, B45.63-67. WHat the"
+
+def indexByBnum(bigarray)
+  for i in 0...bigarray[1].length
+  end
+end
+
 allinfo=stripInfo filename
-writeToXls allinfo
+writeToXls(allinfo, "CatNum")
   
