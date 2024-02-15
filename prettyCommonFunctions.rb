6a2bd8a4f6fbeb3aca2a71b1db735993f2eec03d
Braeden <singleton1@kenyon.edu>
2024-02-14 19:00:26 -0500

    only hashes so far

---
+++ prettyCommonFunctions.rb
@@ -0,0 +1,112 @@
+#This file is meant to be loaded at the start of more specific files.
+# It contains functions that we will use a lot in different applications.
+
+#the first one takes a slide range as a string and outputs a list of all the slides in that range.
+def parseSlideRange(string)
+    #this will be our array that we return at the end
+    slidesMentioned=Array.new
+  
+    collectionsMentioned=Array.new
+    collectionsToIndex=Hash.new
+  
+    #we begin by splitting the ranges. 
+    #Ranges are separated by commas, and common info is not repeated
+    #an especially complicated example of this is 
+    #"B27.012-15, B45.905-06, B47.654-63, 716-18"
+    if string.include? ". "
+        n=string.index ". "
+        string=string[...n]
+    end
+  
+    ranges=string.split(",",-1)
+    
+    for i in 0...ranges.length
+        ranges[i] = ranges[i].lstrip
+    end
+    #next we store each B-collection in case the next one reuses it
+    lastcollection="ERROR"
+  
+    #we now loop through the ranges and process them
+    for i in 0...ranges.length
+      range=ranges[i]
+      
+      #the following will be a sample range to indicate which parts the code is handling
+      
+      #B22.222-22  
+      #   ^
+      if range.include? "."
+        decimalpoint=range.index "."
+        nextpoint=decimalpoint+1
+        rightside=range[nextpoint..]
+      else
+        #in case it is only 222-22 
+        decimalpoint=0
+        rightside=range
+      end
+      
+      #B22.222-22
+      #^^^
+  
+      if range[0]=="B"
+        lastcollection=range[...decimalpoint]
+        unless collectionsMentioned.include? lastcollection
+          collectionsMentioned.push lastcollection
+        end 
+      end
+      
+      #B22.222-22
+      #       ^
+      if rightside.include? "-"
+        dashplace=rightside.index "-"
+      
+        if dashplace < 3
+          rightside="0" + rightside
+          if dashplace < 2
+            rightside="0"+rightside
+          end
+        end
+        dashplace=4
+        hundreds=rightside[0]
+        start=rightside[1..2].to_i
+        last=rightside[dashplace..].to_i
+        if last.to_s.length > 2
+            last=(last-hundreds.to_i*100)
+        end
+        puts [rightside,hundreds,start,last] 
+        for i in start..last
+            if i < 100
+                slidestem=lastcollection + "." + hundreds
+            else 
+                slidestem=lastcollection+"."+(i/100+hundreds.to_i).to_s
+                i=i%100
+            end
+          if i.to_s.length < 2
+            ending="0"+i.to_s
+          else 
+            ending=i.to_s
+          end
+          slide=slidestem+ending
+          slidesMentioned.push slide
+        end
+      else 
+        slide=lastcollection+"."+rightside
+        slidesMentioned.push slide
+      end
+    end
+  
+    slidesMentioned=slidesMentioned.sort
+    minslide=slidesMentioned[0]
+    maxslide=slidesMentioned[-1]
+    return [slidesMentioned,minslide,maxslide]
+    #we begin by splitting our description up by subcollection. 
+end
+=begin The following is a debug routine that allows you to repeatedly test ranges
+s=""
+puts "a debug session has started. enter \"n\" at any time to end it"
+while s != "n"
+  unless s==""
+    puts parseSlideRange(s)
+  end
+  s=gets
+end
+=end
